cd62845cb7880e8e444dd3921dd48f2cd824f298
Aniket24G <94295506+Aniket24G@users.noreply.github.com>
2024-05-29 11:47:18 +0530

    Contact form

--- My-Portfolio/src/Components/Contact/Contact.jsx
+++ My-Portfolio/src/Components/Contact/Contact.jsx
@@ -10,25 +10,45 @@ function Contact() {
         <div className="grid md:grid-cols-2 gap-4 w-full py-2">
           <div className="flex flex-col">
             <label className="uppercase text-sm font-noto">Name</label>
-            <input className="border-2 rounded-lg p-3 border-gray-200 flex" type="text" name="name" />
+            <input
+              className="border-2 rounded-lg p-3 border-gray-200 flex"
+              type="text"
+              name="name"
+            />
           </div>
           <div className="flex flex-col">
             <label className="uppercase text-sm font-noto">Phone Number</label>
-            <input className="border-2 rounded-lg p-3 border-gray-200 flex"  type="text" name="phone" />
+            <input
+              className="border-2 rounded-lg p-3 border-gray-200 flex"
+              type="text"
+              name="phone"
+            />
           </div>
         </div>
-        <div className="flex flex-col">
-          <label className="uppercase text-sm font-noto">Email</label>
-          <input className="border-2 rounded-lg p-3 border-gray-200 flex"  type="email" name="email" />
+        <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+          <div className="flex flex-col">
+            <label className="uppercase text-sm font-noto">Email</label>
+            <input
+              className="border-2 rounded-lg p-3 border-gray-200 flex"
+              type="email"
+              name="email"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="uppercase text-sm font-noto">Subject</label>
+            <input
+              className="border-2 rounded-lg p-3 border-gray-200 flex"
+              type="text"
+              name="subject"
+            />
+          </div>
         </div>
-        <div>
-          <label className="uppercase text-sm font-noto">Subject</label>
-          <input className="border-2 rounded-lg p-3 border-gray-200 flex"  type="text" name="subject" />
-        </div>
-        <div>
+
+        <div className="flex flex-col py-2">
           <label className="uppercase text-sm font-noto">Message</label>
-          <textarea name="message" id="" cols="30" rows="10"></textarea>
+          <textarea className="border-2 rounded-lg p-3 border-gray-200" name="message" id="" cols="30" rows="10"></textarea>
         </div>
+        <button className="bg-primaryText text-gray-200 font-noto mt-4 p-4 rounded-lg w-1/4">Send</button>
       </form>
     </div>
   );
