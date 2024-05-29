bb4f956e791ee44dc3c6709666508f29e1915ef1
Aniket24G <94295506+Aniket24G@users.noreply.github.com>
2024-05-29 12:13:07 +0530

    Final draft

--- My-Portfolio/src/Components/Navbar/Navbar.jsx
+++ My-Portfolio/src/Components/Navbar/Navbar.jsx
@@ -49,14 +49,6 @@ function Navbar() {
           </a>
           <a
             onClick={handleNav}
-            href="#resume"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-md bg-gray-200 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
-          >
-            <BsPerson size={20} />
-            <span className="pl-4">Resume</span>
-          </a>
-          <a
-            onClick={handleNav}
             href="#contact"
             className="w-[75%] flex justify-center items-center rounded-full shadow-md bg-gray-200 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-150"
           >
@@ -87,12 +79,7 @@ function Navbar() {
           >
             <GrProjects size={20} />
           </a>
-          <a
-            href="#resume"
-            className="rounded-full shadow-md bg-gray-200 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
-          >
-            <BsPerson size={20} />
-          </a>
+          
           <a
             href="#contact"
             className="rounded-full shadow-md bg-gray-200 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
